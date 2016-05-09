6579371e40a676e2dd0b1e4eec3f376bae802a12
LeonardoTech <2416751962@qq.com>
2016-05-09 13:55:38 +0800

    a

---
+++ ProjectUsage/IQuadPrimitive.h
@@ -0,0 +1,45 @@
+﻿
+#ifndef __IQUAD_PRIMITIVE_H__
+#define __IQUAD_PRIMITIVE_H__
+#include "IDrawElement.h"
+
+/// @class	IQuadPrimitives
+///
+/// @brief	 <绘制面元的接口，继承于IDrawElement和IColorable>.
+///
+/// @author	Admin
+/// @date	2016/3/23
+class IQuadPrimitive :public IDrawElement
+{
+public:
+
+	/// @fn	virtual void IQuadPrimitive::setVertices(Vertex vet1, Vertex vet2, Vertex vet3, Vertex vet4) = 0;
+	///
+	/// @brief	 <设置顶点的坐标>.
+	///
+	/// @author	Admin
+	/// @date	2016/3/28
+	///
+	/// @param	vet1	<第一个顶点坐标>.
+	/// @param	vet2	<第二个顶点坐标>.
+	/// @param	vet3	<第三个顶点坐标>.
+	/// @param	vet4	<第四个顶点坐标>.
+	virtual void setVertices(Vertex vet1, Vertex vet2, Vertex vet3, Vertex vet4) = 0;
+
+	/// @fn	virtual void IQuadPrimitive::getVertices(Vertex& vet1, Vertex& vet2, Vertex& vet3, Vertex& vet4) = 0;
+	///
+	/// @brief	 <获取顶点的坐标>.
+	///
+	/// @author	Admin
+	/// @date	2016/3/28
+	///
+	/// @param [in,out]	<第一个顶点坐标>.
+	/// @param [in,out]	<第二个顶点坐标>.
+	/// @param [in,out]	<第三个顶点坐标>.
+	/// @param [in,out]	<第四个顶点坐标>.
+	virtual void getVertices(Vertex& vet1, Vertex& vet2, Vertex& vet3, Vertex& vet4) = 0;
+};
+
+
+
+#endif // __IQUAD_PRIMITIVE_H__
